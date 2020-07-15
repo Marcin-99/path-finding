e95c8e25ebbf91390b3cbc6efc09ea6c70909602
Marcin-99 <MarcinAszyk@o2.pl>
2020-07-15 23:18:43 +0200

    Added dummy example of fetching grid from python code.

--- path_finding_react/src/ProjectVisualizer/Visualizer.js
+++ path_finding_react/src/ProjectVisualizer/Visualizer.js
@@ -83,10 +83,22 @@ class Visualizer extends React.Component {
   	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+  	fetchBestFirst = () => {
+  		console.log("fetching...");
+
+    	fetch('http://127.0.0.1:8000/algorithms/best-first/width=8&height=8&start=0,4&goal=7,5&walls=1,5&1,7&2,5&2,7&3,5&3,7&4,4&4,5&4,7&5,4&5,7&6,5&6,6&6,7')
+      		.then(response => response.json())
+      		.then(data => 
+          		console.log(data)
+        	)
+  	}
+
+
   	visualizeBestFirst() {
   		const {nodes} = this.state;
   		const startNode = nodes[START_NODE_ROW][START_NODE_COL];
   		const finishNode = nodes[FINISH_NODE_ROW][FINISH_NODE_COL];
+  		this.fetchBestFirst();
   		//const visitedNodesInOrder = bestFirst(nodes, startNode, finishNode);
   	}
 
