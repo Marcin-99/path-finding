4c6134dfa755f23bf4af47f61510ebeeb46dbf85
Marcin-99 <MarcinAszyk@o2.pl>
2020-07-24 14:33:12 +0200

    Added basic routing. Changed structure of the react project.

--- path_finding_react/src/App.js
+++ path_finding_react/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
-import Visualizer from './ProjectVisualizer/Visualizer';
+import Visualizer from './Components/ProjectVisualizer/Visualizer';
+import NavBar from './Components/NavBar/navBar';
+import Mazes from './Components/MazesPage/mazes';
 
 
 function App() {
   return (
     <div className="App">
-      <Visualizer />
+    	<BrowserRouter>
+	    	<NavBar />
+	      	<Route path="/visualizer" component={ Visualizer } />
+	      	<Route path="/mazes" component={ Mazes } />
+      	</BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
